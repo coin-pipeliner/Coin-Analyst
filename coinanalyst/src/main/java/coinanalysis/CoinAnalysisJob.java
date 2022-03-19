7b02f8afe5b472f7d92b4690d078f15e9d79233a
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2022-03-19 23:59:32 +0900

    Add flink application base code

---
+++ coinanalyst/src/main/java/coinanalysis/CoinAnalysisJob.java
@@ -0,0 +1,15 @@
+package coinanalysis;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.walkthrough.common.sink.AlertSink;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+import org.apache.flink.walkthrough.common.source.TransactionSource;
+
+public class CoinAnalysisJob {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    }
+}
