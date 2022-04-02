5160a50502d7e4e7a61e35092a20ec38556a0aab
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2022-04-02 19:17:16 +0900

    fix: Edit records json mapping

--- coinanalyst/src/main/java/coinanalysis/records/Ticker.java
+++ coinanalyst/src/main/java/coinanalysis/records/Ticker.java
@@ -1,7 +1,6 @@
 package coinanalysis.records;
 
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.annotation.JsonNaming;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Date;
 import java.util.Objects;
@@ -12,42 +11,69 @@ import java.util.Objects;
  * 아래 문서를 참고하시길 바랍니다.
  * https://docs.upbit.com/docs/upbit-quotation-websocket#%ED%98%84%EC%9E%AC%EA%B0%80ticker-%EC%9D%91%EB%8B%B5
  */
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Ticker {
-
 
     private String type;
     private String code;
+    @JsonProperty("opening_price")
     private double openingPrice;
+    @JsonProperty("high_price")
     private double highPrice;
+    @JsonProperty("low_price")
     private double lowPrice;
+    @JsonProperty("trade_price")
     private double tradePrice;
+    @JsonProperty("prev_closing_price")
     private double prevClosingPrice;
     private String change;
+    @JsonProperty("change_price")
     private double changePrice;
+    @JsonProperty("signed_change_price")
     private double signedChangePrice;
+    @JsonProperty("change_rate")
     private double changeRate;
+    @JsonProperty("signed_change_rate")
     private double signedChangeRate;
+    @JsonProperty("trade_volume")
     private double tradeVolume;
+    @JsonProperty("acc_trade_volume")
     private double accTradeVolume;
+    @JsonProperty("acc_trade_volume_24h")
     private double accTradeVolume24H;
+    @JsonProperty("acc_trade_price")
     private double accTradePrice;
+    @JsonProperty("acc_trade_price_24h")
     private double accTradePrice24H;
+    @JsonProperty("trade_date")
     private Date tradeDate;
+    @JsonProperty("trade_time")
     private Date tradeTime;
+    @JsonProperty("trade_timestamp")
     private long tradeTimestamp;
+    @JsonProperty("ask_bid")
     private String askBid;
+    @JsonProperty("acc_ask_volume")
     private double accAskVolume;
+    @JsonProperty("acc_bid_volume")
     private double accBidVolume;
+    @JsonProperty("highest_52_week_price")
     private double highest52WeekPrice;
+    @JsonProperty("highest_52_week_date")
     private Date highest52WeekDate;
+    @JsonProperty("lowest_52_week_price")
     private double lowest52WeekPrice;
+    @JsonProperty("lowest_52_week_date")
     private Date lowest52WeekDate;
+    @JsonProperty("market_state")
     private String marketState;
+    @JsonProperty("is_trading_suspended")
     private boolean isTradingSuspended;
+    @JsonProperty("delisting_date")
     private Date delistingDate;
+    @JsonProperty("market_warning")
     private String marketWarning;
     private long timestamp;
+    @JsonProperty("stream_type")
     private String streamType;
 
 
