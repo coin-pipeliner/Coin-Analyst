3661f7ef938d201d23502728ceb02f155501648d
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2022-04-05 22:42:36 +0900

    feature: Test base code

---
+++ coinanalyst/src/test/java/coinanalysis/util/TestCoinAnalyzer.java
@@ -0,0 +1,31 @@
+package coinanalysis.util;
+
+import coinanalysis.records.Ticker;
+import coinanalysis.records.TickerDeserializationSchema;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class TestCoinAnalyzer {
+
+    @Test
+    void tickerDeserializationTest() throws IOException {
+        Ticker expectedTicker = new Ticker(
+                /*
+                * TODO
+                *  Insert expected ticker data
+                * */
+        );
+
+
+        TickerDeserializationSchema tickerDeserializationSchema = new TickerDeserializationSchema();
+
+        byte[] tickerData = TestTickerData.TICKERS[0].getBytes();
+        Ticker ticker = tickerDeserializationSchema.deserialize(tickerData);
+
+
+        Assertions.assertEquals(expectedTicker, ticker);
+
+    }
+}
